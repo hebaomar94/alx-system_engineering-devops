c2f3b9d5f773f9cb905874cf9bb5befe65bb3f50
hebaomar94 <hebaomar93.ho@gmail.com>
2023-03-24 18:57:53 +0000

    1-install_a_package.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
-# This puppet manifest creates a file names/tmp/school
-file { '/tmp/school':
-  ensure         => file,
-  mode           => '0744',
-  owner          => 'www-data',
-  group          => 'www-data',
-  content        => 'I love Puppet\n',
-  ensure_newline => true,
+# create a file in /tmp
+
+file { 'school':
+  ensure  => 'file',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
